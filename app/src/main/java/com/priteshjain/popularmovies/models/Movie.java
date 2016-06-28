ad11ec135a0381dba453874ac3923b6938c3fc15
Pritesh Jain <prit.jain86@gmail.com>
2016-06-28 18:09:15 +0530

    add movie model

---
+++ app/src/main/java/com/priteshjain/popularmovies/models/Movie.java
@@ -0,0 +1,12 @@
+package com.priteshjain.popularmovies.models;
+
+public class Movie {
+    private String id;
+    private String title;
+    private String originalTitle;
+    private String overview;
+    private String releaseDate;
+    private String posterPath;
+    private String backdropPath;
+    private double voteAverage;
+}
