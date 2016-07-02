a8c7f56fa99e13d6a07488a734cbb4a58f1d2416
Pritesh Jain <prit.jain86@gmail.com>
2016-07-02 18:57:48 +0530

    Load more added

--- app/src/main/java/com/priteshjain/popularmovies/constants/ApiEndpoint.java
+++ app/src/main/java/com/priteshjain/popularmovies/constants/ApiEndpoint.java
@@ -3,12 +3,13 @@ package com.priteshjain.popularmovies.constants;
 import com.priteshjain.popularmovies.models.PaginatedMovie;
 
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 import rx.Observable;
 
 public interface ApiEndpoint {
     @GET("/3/movie/popular")
-    Observable<PaginatedMovie> getPopularMovies();
+    Observable<PaginatedMovie> getPopularMovies(@Query("page") String page);
 
     @GET("/3/movie/top_rated")
-    Observable<PaginatedMovie> getTopRatedMovies();
+    Observable<PaginatedMovie> getTopRatedMovies(@Query("page") String page);
 }
